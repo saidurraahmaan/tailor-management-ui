ce4a499829c8ebf2a4df305bdd392ed1c7fb4866
saidur-dsi <saidur.rahman@dsinnovators.com>
2023-10-06 12:35:02 +0600

    order list data table implemented

--- src/utils/ApiCustomHook.jsx
+++ src/utils/ApiCustomHook.jsx
@@ -1,33 +1,35 @@
 import { useState, useEffect } from "react";
 import HttpInstance from "../config/axiosClient";
+import { STATUS } from "../constants/fetch";
 
 const useApiHook = (method, url, data = null, errorHandler = null) => {
   const [responseData, setResponseData] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [fetchStatus, setFetchStatus] = useState(STATUS.IDLE);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
+      setFetchStatus(STATUS.LOADING);
       try {
         const response = await HttpInstance.request({
           method,
           url,
           data,
-        }); 
+        });
+        setFetchStatus(STATUS.SUCCESS);
         setResponseData(response.data);
       } catch (error) {
         setError(error);
+        setFetchStatus(STATUS.ERROR);
         if (errorHandler) {
           errorHandler(error);
         }
-      } finally {
-        setLoading(false);
       }
     };
     fetchData();
   }, [method, url, data, errorHandler]);
 
-  return { responseData, loading, error };
+  return { responseData, fetchStatus, error };
 };
 
 export default useApiHook;
